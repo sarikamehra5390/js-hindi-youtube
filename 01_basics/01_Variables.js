69607f1a775b6b19ae1b793e520ed061ccc83e62
sarikamehra5390 <142200497+sarikamehra5390@users.noreply.github.com>
2024-10-09 06:28:36 +0000

    Included DataTypes file

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,4 @@
+const accountId = 144553;
+let accountEmail = "sarika@5437.com";
+var accountPassword = "12345";
+accountCity = "Jaipur"; 
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+3);
+//code readability should be high 
+console.log("Sarika");
+
+let name ="Sarika";
+let age = 18;
+let isLoggedIn = false ;
+
+// number => 2 to the power of 53
+// bigint => when the number is very large eg: trading etc.
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object 
+
+console.log(typeof "sarika");
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
