3dca8d7c3a46965b8f146222f4cb802650971295
Sarika Mehra <mehrasarika574@gmail.com>
2024-12-23 13:12:12 +0530

    modified conversionOperater file

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,37 +1,43 @@
 let score = "33" ;
 
-// console.log(typeof score);
-// console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
 let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN
-// true =. 1 ; false => 0;
+// "33" => 33 , typeof =>number
+// "33abc" => NaN , typeof =>number
+// true = 1 ; false => 0;
 
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-// console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
-// "hitesh" => true
+// "Sarika" => true
 
 let someNumber = 33 ;
 
 let stringNumber = String(someNumber);
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
 
-// **************** Operations *****************
+// **************** Operations ******************
 
 let value = 3;
 let negValue = -value;
 console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Sarika";
+let str3 = str1 + str2;
+console.log(str3);
+
 
 console.log(2+3**2);
 
