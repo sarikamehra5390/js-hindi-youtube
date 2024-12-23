cb5c085ed9dc8213191c73d7a99df2380e281368
Sarika Mehra <mehrasarika574@gmail.com>
2024-12-23 13:13:55 +0530

    conversionOperator file is modified

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -37,13 +37,19 @@ let str1 = "Hello";
 let str2 = " Sarika";
 let str3 = str1 + str2;
 console.log(str3);
+// Concate the strings 
 
 
 console.log(2+3**2);
 
-console.log("1" + 2);
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); //32
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
 
 
 
