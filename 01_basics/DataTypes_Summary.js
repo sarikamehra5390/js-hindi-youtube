639b786a6398d6b1e0a6a8426b65d6f9a6c2f8fb
Sarika Mehra <mehrasarika574@gmail.com>
2024-12-24 11:38:06 +0530

    file is modified

--- 01_basics/DataTypes_Summary.js
+++ 01_basics/DataTypes_Summary.js
@@ -2,17 +2,26 @@
 // primitive and non- primitive
 
 //  #primitive
-// 7 types : String , numbers ,boolean , null , undefined , symbol, BigInt
+// 7 types : String , numbers ,boolean , null , undefined , symbol(used to make a value unique), BigInt(large values are handled)
+// primitive datatypes are call by value (copy the value , changes are only applied in the copies )
 
-const score = 100; // In js we does not define the language 
+const score = 100; 
+const scoreValue = 100.8 ;
 
-const id = Symbol('123');
+const isLoggedIn = false;
+const outSideTemp = null ;
+let userEmail; // undefined
+
+// In js we does not define the language
+// js is dynamically typed(This means that variable types are determined at runtime , we don't need to specify a variable's type when declaring it . The type of a variable can change during execution depending on the value it is assigned ) 
+
+const id = Symbol('123'); 
 const anotherId = Symbol('123');
 
 console.log(id);
 console.log(anotherId);
 
-console.log(id === anotherId);
+console.log(id === anotherId); // false
 
 const bigNumber = 345636853798368736n;
 
@@ -23,14 +32,25 @@ const bigNumber = 345636853798368736n;
 // # non-primitive (Reference)
 // Array , Objects , Functions
 
-const heroes =  ["Shaktiman" , "naagraj" , "doga"];
+const heroes =  ["Shaktiman" , "naagraj" , "doga"]; //Arrays
 
+//Objects
 let myObj = {
     name:"hitesh",
     age : 22,
 }
 
+//Functions
  const myFunction = function(){
     console.log("Hello World");
 
 }
+
+console.log("Return type of all the dataTypes : ")
+console.log(typeof scoreValue);
+console.log(typeof isLoggedIn);
+console.log(typeof outSideTemp);
+console.log(typeof id);
+console.log(typeof heroes);
+console.log(typeof myObj);
+console.log(typeof myFunction);
