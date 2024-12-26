73e6dfadde5acf89516a2fedbd07ed3ba3924861
Sarika Mehra <mehrasarika574@gmail.com>
2024-12-26 11:32:46 +0530

    Date file is added

---
+++ 01_basics/07_DateInJs.js
@@ -0,0 +1,27 @@
+let myDate = new Date();
+
+console.log(myDate.toString());//Wed Dec 25 2024 11:56:09 GMT+0530 (India Standard Time)
+console.log(myDate.toISOString()); //2024-12-25T06:26:09.901Z
+console.log(myDate.toJSON()); //2024-12-25T06:26:09.901Z
+console.log(myDate.toLocaleDateString()); // 12/25/2024
+console.log(myDate.toLocaleString()); // 12/25/2024, 11:56:09 AM
+console.log(myDate.toLocaleTimeString());// 11:56:09 AM
+console.log(myDate.toDateString()); //Wed Dec 25 2024
+console.log(myDate.getTimezoneOffset()); // -330
+console.log(myDate.toUTCString()); //Wed, 25 Dec 2024 06:26:09 GMT
+
+console.log(typeof myDate); // Object 
+
+let myCreateDate = new Date(2023 , 0 , 13);
+let myCreateDate1 = new Date("2023-01-14");
+console.log(myCreateDate.toDateString()); // Fri Jan 13 2023
+console.log(myCreateDate1.toLocaleString()); //1/14/2023, 5:30:00 AM
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+
