726df5f1a1468993db2c6b83e3e52f02e6f328a9
Sarika Mehra <mehrasarika574@gmail.com>
2024-12-30 10:02:12 +0530

    Function file is modified

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -46,5 +46,57 @@ function loginUserMessage(userName){
 loginUserMessage("Sarika"); // This will only return the value 
 console.log(loginUserMessage("Sarika")); //It will return and print the value 
 
+// this will not take multiple values as an argument 
+function calculateCarPrice1(num1){
+     return num1;
+}
+
+console.log(calculateCarPrice1(2)); //2
+
+//Rest operator is used in case of not knowing how many arguments are going to be there 
+//rest and spread operator syntax is same  but it depends on its use case that when we have to call it spred or rest 
+
+function calculateCarPrice2(...num1){ // Rest is used here 
+    return num1;
+}
+
+// It will return the output in the form of array of all the arguments given 
+console.log(calculateCarPrice2(200 , 300 , 400 , 2000)); // [ 200, 300, 400, 2000 ]
+
+
+function calculateCarPrice3( val1 , val2 , ...num1){ // Rest is used here 
+    return num1;
+}
+
+console.log(calculateCarPrice3(200 , 300 , 500 , 2000)); // [ 500, 2000 ]
+
+const user = {
+    userName : "Sarika ",
+    prices : 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and the price is ${anyObject.prices}`); 
+    
+}
+
+handleObject(user); // Username is Sarika  and the price is 199
+
+const myNewArray = [200 , 300 , 400 , 100];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
+
+
+
+
+
+
+
 
 
