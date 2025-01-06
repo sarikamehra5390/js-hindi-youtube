29551696356fd396e29a05265521ec8e8cf69cb0
Sarika Mehra <mehrasarika574@gmail.com>
2025-01-06 11:40:44 +0530

    loops file is added

---
+++ 05_Iterations.js/Two.js
@@ -0,0 +1,27 @@
+// while (condition) {
+    
+// }
+
+let index = 0;
+while(index <= 10){
+    console.log(`Value of index is ${index}`);
+    index += 2;
+}
+
+let myArray = ["Flash" , "batman" , "Superman"];
+
+let arr = 0 ;
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+let score = 11 ;
+do {
+    console.log(`score is ${score}`);
+    score++;
+}while(score <= 10);
+
+//In do while loop condition is checked after printing 
+
+
