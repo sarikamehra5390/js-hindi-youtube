1e23ec673a491ea8bc3f295209ebe32bce7399f9
Sarika Mehra <mehrasarika574@gmail.com>
2026-01-19 21:33:40 +0530

    new projects added

---
+++ UnlimitedColor/chaiaurcode.js
@@ -0,0 +1,30 @@
+// generate a random color
+
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+const startChangingColor = function () {
+  if (intervalId == null) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null; // we are dereferencing the value of intervalId .
+};
+
+document.querySelector('#start').addEventListener('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
