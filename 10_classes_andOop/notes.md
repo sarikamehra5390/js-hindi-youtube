45caba4ff85869714e76127ebf08ecf26aa6fcb3
Sarika Mehra <mehrasarika574@gmail.com>
2026-03-22 22:56:24 +0530

    added classes and oops content

---
+++ 10_classes_andOop/notes.md
@@ -0,0 +1,24 @@
+# Javascript and classes 
+
+## javascript is a prototype - based language 
+## OOP is a programming paradigm
+
+## Object ->
+- collection of properties and methods
+- toLowerCase
+
+## Why we use OOP ?
+
+## parts of OOP
+Object Literal 
+
+-constructor functions
+-prototypes
+-Classes
+-Instances (new , this)
+
+## 4 pillars of OOPs
+-Abstraction
+-Encapsulation
+-Inheritance 
+-Polymorphism
--- 10_classes_andOop/oop.js
+++ 10_classes_andOop/oop.js
@@ -19,18 +19,33 @@ console.log(user.getUserDetails());
 
 //Constructor function
 
-const promiseOne = new Promise()
-const date = new Date()
+const promiseOne = new Promise();
+const date = new Date();
 
 function User(username , loginCount ,isLoggedIn){
     this.username = username ;
     this.loginCount = loginCount;
     this.isLoggedIn  = isLoggedIn;
 
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+    }
+
     return this;
 }
 
-const userOne = User("Sarika" , 12 , true);
+const userOne = new User("Sarika" , 12 , true);
 
-console.log(userOne);
+const userTwo = new User("Chai aur code " , 11 , false);
+
+//userTwo will override the value of userOne
+
+console.log(userOne.constructor);
+
+//new is the comstructor function
+//new keyword - whenever we use new keyword an empty object is created which ic called an instance 
+// 1. A new object has been created
+//2. constructor function is called using new 
+// 3. all the arguments written are injected in this keyword
+
 
